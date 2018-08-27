871977a3cc2339c9c3e629706c08777b0095d94f
WheatleyTheCore <alex.colwell@icloud.com>
2018-08-27 17:38:10 -0400

    now links between running instances

--- src/App.js
+++ src/App.js
@@ -41,6 +41,12 @@ class SidePortal extends Component {
   componentWillUnmount() {
     sidenavelement.removeChild(this.el);
   }
+
+  componentDidUpdate() {
+    if (window.location.pathname == "/ang") {
+      window.location.href = "http://127.0.0.1:3001";
+    }
+  }
   render() {
     return ReactDOM.createPortal(
       this.props.children,
@@ -51,13 +57,13 @@ class SidePortal extends Component {
 
 const Home = () => (
   <div>
-    <h2>Home</h2>
+    <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h2>
   </div>
 )
 
 const About = () => (
   <div>
-    <h2>About</h2>
+    <h2>This is an About page. We keep the Abouts here.</h2>
   </div>
 )
 
@@ -81,16 +87,16 @@ class App extends Component {
   }
 }
 const TopNav = () => (
-  <div className="nav-container">
-      <div className="nav-icon"><Link to="/">topHome</Link></div>
-      <div className="nav-icon"><Link to="/about">topAbout</Link></div>
+  <div className="top-nav-container">
+      <div className="top-nav-icon"><Link to="/">Just React</Link></div>
+      <div className="top-nav-icon"><Link to="/ang">React with Angular</Link></div>
   </div>
 )
 
 const SideNav = () => (
-  <div className="nav-container">
-      <div className="nav-icon"><Link to="/">sideHome</Link></div>
-      <div className="nav-icon"><Link to="/about">sideAbout</Link></div>
+  <div className="side-nav-container">
+      <div className="side-nav-icon"><Link to="/">sideHome</Link></div>
+      <div className="side-nav-icon"><Link to="/about">sideAbout</Link></div>
   </div>
 )
 
